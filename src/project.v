46762ece2f965068bac7f3327a3fc9ccd15fd07c
Niklaus Leuenberger <nleuenberger@zumbach.ch>
2026-09-21 08:40:22 +0200

    src: wrap power pins in conditional

--- src/project.v
+++ src/project.v
@@ -6,8 +6,10 @@
 `default_nettype none
 
 module tt_um_nikleberg_mixer (
+`ifdef USE_POWER_PINS
     input  wire       VGND,
     input  wire       VDPWR,    // 1.2v power supply
+`endif
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
